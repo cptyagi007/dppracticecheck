ff64baf8c90cedc3f23f1db2bf228ef72a2d4105
849728 <63786225+cptyagi007@users.noreply.github.com>
2020-05-05 18:52:57 +0530

    Add files via upload

--- NotificationService.java
+++ NotificationService.java
@@ -3,43 +3,42 @@ package com.clas.implementation;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.interfaces.contract.INotificationObserver;
+import com.interfaces.contract.NotificationObserver;
 import com.interfaces.contract.INotificationService;
 
 public class NotificationService implements INotificationService {
 
-	private List<INotificationObserver> observers;
-	private boolean changed;
+	private List<NotificationObserver> observers;
 	
 	public NotificationService(){
 		this.observers=new ArrayList<>();
 	}
 	
-	public void AddSubscriber(INotificationObserver ob) {
+	public void AddSubscriber(NotificationObserver ob) {
 		if(ob == null) throw new NullPointerException("Null Observer");
 		
 		if(!observers.contains(ob)) {
 			
 			observers.add(ob);
-			for(INotificationObserver o:observers)
+			for(NotificationObserver o:observers)
 			   System.out.println(o);
 		}
 		
 	}
 	
-	public void RemoveSubscriber(INotificationObserver ob) {
+	public void RemoveSubscriber(NotificationObserver ob) {
 		
 		observers.remove(ob);
-		for(INotificationObserver o:observers)
+		for(NotificationObserver o:observers)
 			   System.out.println(o);
 		
 	}
 	 public void NotifySubscriber() {
-		 List<INotificationObserver> observersLocal = null;
+		 List<NotificationObserver> observersLocal = null;
 			
 			observersLocal = new ArrayList<>(this.observers);
-			for (INotificationObserver obj : observersLocal) {
-				obj.OnServerDown();
+			for (NotificationObserver obj : observersLocal) {
+				obj.sendMessage();
 			}
 
 	 }
